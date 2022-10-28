e2c5702cd435bdd371bdd636c57d24571df6f3ba
Andrielson Silva <andrielson@gmail.com>
2022-10-28 14:58:26 -0300

    Add Context to Schema and Server

--- src/schema.ts
+++ src/schema.ts
@@ -6,6 +6,10 @@ export const schema = makeSchema({
     types,
     outputs: {
         schema: join(process.cwd(), "schema.graphql"),
-        typegen: join(process.cwd(), "nexus-typegen.ts")
-    }
+        typegen: join(process.cwd(), "nexus-typegen.ts"),
+    },
+    contextType: {
+        module: join(process.cwd(), "./src/context.ts"),
+        export: "Context",
+    },
 });
