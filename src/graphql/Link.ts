aa41db65fda27c90abb8ca151e28d347b2f2cd28
Andrielson Silva <andrielson@gmail.com>
2022-10-28 16:33:26 -0300

    Update Link schema

--- src/graphql/Link.ts
+++ src/graphql/Link.ts
@@ -7,6 +7,14 @@ export const Link = objectType({
         t.nonNull.int("id");
         t.nonNull.string("description");
         t.nonNull.string("url");
+        t.field("postedBy", {
+            type: "User",
+            resolve({id}, _, {prisma}) {
+                return prisma.link
+                    .findUnique({ where: {id}})
+                    .postedBy();
+            },
+        });
     },
 });
 
